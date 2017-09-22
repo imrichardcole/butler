856efbb637d1673efa4035f80307a3019618b6c6
Richard Cole <richandkerrycole@gmail.com>
2017-09-22 11:58:48 +0100

    fix up test

--- src/test/java/uk/co/imrichardcole/butler/config/MonitorConfigParserTest.java
+++ src/test/java/uk/co/imrichardcole/butler/config/MonitorConfigParserTest.java
@@ -13,17 +13,11 @@ public class MonitorConfigParserTest {
     public void can_load_config_from_file() {
         final MonitorConfigParser parser = new MonitorConfigParser();
         final Map<String, MonitorConfig> configMap = parser.parse("basic-example.conf");
-        assertThat(configMap.size(), equalTo(2));
-
+        assertThat(configMap.size(), equalTo(1));
         final MonitorConfig memoryMonitorConfig = configMap.get("JMX memory logger");
         assertThat(memoryMonitorConfig.getFileName(), equalTo("memory-stats.csv"));
         assertThat(memoryMonitorConfig.getOutputFrequencyMillis(), equalTo(1000L));
         assertThat(memoryMonitorConfig.getObjectName(), equalTo("java.lang:type=Memory"));
-
-        final MonitorConfig threadMonitorConfig = configMap.get("JMX thread logger");
-        assertThat(threadMonitorConfig.getFileName(), equalTo("thread-stats.csv"));
-        assertThat(threadMonitorConfig.getOutputFrequencyMillis(), equalTo(2000L));
-        assertThat(threadMonitorConfig.getObjectName(), equalTo("java.lang:type=Threading"));
     }
 
 }
